ea25401f12febaa47628225f582110f8042dddf7
momo <92312250+MomokaHara@users.noreply.github.com>
2021-10-31 06:08:42 -0700

    create memoDetailScreen

--- App.jsx
+++ App.jsx
@@ -1,28 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 
-import AppBar from './src/components/AppBar';
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/components/CircleButton';
+import MomoListScreen from './src/screens/MemoListScreen';
+import MomoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <AppBar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
-    </View>
-  );
+  return <MomoDetailScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F4F4F8',
-    // 左詰：flex-start, 右詰：flex-end
-    // alignItems: 'center',
-    // 上詰：flex-start, 下詰：flex-end
-    // justifyContent: 'center',
-  },
-});
